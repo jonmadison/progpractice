8b21328eed5cc567b4df91a699f69b788f687a3a
jon madison <jon@tfftech.com>
2015-03-24 18:29:00 -0700

    sieve within a range

--- js/primesieve.js
+++ js/primesieve.js
@@ -1,19 +1,22 @@
 var primesSieve = function(lowerRange,upperRange) {
 	var primes = new Array(upperRange)
+	var result = []
 	for(var i = 0; i < upperRange; i++) {
 		primes[i] = 1
 	}
 
-	for(var i = lowerRange; i < primes.length; i++) {
+	for(var i = 2; i < primes.length; i++) {
 		if(primes[i]==0) continue
-			console.log(i + " is prime.")
-			// console.log("found prime: " + i + ", removing factors from " + primes)
-			for(j = 0; i*j <= primes.length; j++) {
-				primes[i*j] = 0
-			}
+		// console.log(i + " is prime.")
+		if(i >= lowerRange && i <= upperRange) result.push(i)
+		// console.log("found prime: " + i + ", removing factors from " + primes)
+		for(j = 0; i*j <= primes.length; j++) {
+			primes[i*j] = 0
+		}
 	}
+	console.log(result)
 }
 
-primesSieve(10,50)
+primesSieve(1,10000000)
 
 module.exports.ps = primesSieve
